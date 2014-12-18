ccfae1cfebd71075188d6819932f7f229bfe4a5e
Youngju <youngju@ecolemo.com>
2014-12-18 11:49:24 +0900

    mysql setting

--- legalcounseling_py3/settings.py
+++ legalcounseling_py3/settings.py
@@ -58,8 +58,15 @@ WSGI_APPLICATION = 'legalcounseling_py3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'legalcounseling',
+        'USER': 'ecolemo',
+        'PASSWORD': 'ecolemo',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
