832edd5de16e281ba4f8aa84b65e78a43c80915e
Youngju <youngju@ecolemo.com>
2014-12-18 11:57:37 +0900

    migrate

--- counsels/models.py
+++ counsels/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=1000)
+    content = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
