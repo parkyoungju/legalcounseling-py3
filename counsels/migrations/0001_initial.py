24440a3193ec4f339200745b2316b5e8d7da2b35
Youngju <youngju@ecolemo.com>
2014-12-22 07:32:58 +0900

    bower installed

---
+++ counsels/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=1000)),
+                ('content', models.TextField(null=True, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- counsels/views.py
+++ counsels/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from counsels.models import Post
 
-# Create your views here.
+
+def index(request):
+    posts = Post.objects.order_by('created')
+    template = loader.get_template('counsels/index.html')
+    context = RequestContext(request, {
+        'posts': posts,
+    })
+    return HttpResponse(template.render(context))
+
+def post(request):
+    return HttpResponse("Post")
